874a3fc61e6a041352773ebe80b31bc43b432ef0
Teemu Haila <teemu.haila@gmail.com>
2025-11-16 11:04:44 +0200

    chore: update dependencies and tooling configuration
    - upgrade all dependencies to latest versions (vite 7, vue 3.5, eslint 9)
    - migrate from eslint 8 to eslint 9 (flat config)
    - switch to tabs and increase printWidth to 120 in prettier
    - separate typecheck from build script
    - add dev/lint/update commands to Claude settings
    - remove deprecated .eslintrc.cjs in favor of flat config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import prettierConfig from '@vue/eslint-config-prettier'
+
+export default defineConfigWithVueTs(
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
+  prettierConfig,
+  {
+    rules: {
+      semi: ['error', 'never'],
+      'comma-dangle': ['error', 'always-multiline'],
+    },
+  },
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+)
